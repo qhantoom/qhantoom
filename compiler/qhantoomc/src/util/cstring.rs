35e0a70ad9d0413b1deb5b3e23d04e22e1fd98da
monsieurbadia <iam@monsieurbadia.com>
2021-09-27 22:44:28 +0200

    [qhantoom]: implement rename cstring macro functions

--- compiler/qhantoomc/src/util/cstring.rs
+++ compiler/qhantoomc/src/util/cstring.rs
@@ -1,11 +1,11 @@
-pub macro cstring($s:expr) {
+pub macro cstr($s:expr) {
   format!("{}{}", $s, "\0").as_ptr() as *const i8
 }
 
-pub macro cstring_mut($s:expr) {
+pub macro cstr_mut($s:expr) {
   format!("{}{}", $s, "\0").as_ptr() as *mut i8
 }
 
-pub macro cstring_mut_mut($s:expr) {
+pub macro cstr_mut_mut($s:expr) {
   format!("{}{}", $s, "\0").as_ptr() as *mut *mut i8
 }
